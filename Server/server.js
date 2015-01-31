eee36314be2596db9aa2f4d9887bfe50d92505a4
bhanuc <bhanuc@iitk.ac.in>
2015-01-31 18:19:30 +0530

    updated server.js

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,8 @@ server.listen(8080);
 
 var wss = new WebSocketServer({server: server});
 wss.on('connection', function(ws) {
-  console.log('started ');
+    console.log(ws);
+  ws.send(JSON.Stringify({'connected': true});
   ws.on('move', function(data, flags) {
     ws.send(JSON.Stringify({'move': data}));
   });
