a293ec390031c4f9fb06d20f9caf2a568bb52585
bhanuc <bhanuc@iitk.ac.in>
2015-01-31 20:43:13 +0530

    fixed JSON. stringify

--- Server/server.js
+++ Server/server.js
@@ -21,85 +21,111 @@ var wss = new WebSocketServer({
 
 wss.on('connection', function (ws) {
     ws.on('message', function (message) {
-            console.log(JSON.stringify(message),message);
-            if ( typeof message != Object) {
-                var mess = JSON.parse(message);
-            }   else {
-                var mess = message;
-            }
-                console.log(mess);
-                
-                if (mess.hasOwnProperty('type')) {
-                    switch (mess.type) {
-                    case "join":
-                        if (room.hasOwnProperty(mess.room)) {
-                            if (room[mess.room].length > 1) {
-                                ws.send({
-                                    "type": "error",
-                                    'error': 'Room is Full'
-                                });
-                            } else {
-                                var length = room[mess.room].length;
-                                room[mess.room][length - 1] = {
-                                    'id': 2,
-                                    'ws': ws
-                                };
-                                for (var i = 0; i < room[mess.room].length; i++) {
-                                    room[mess.room][i].ws.send({
-                                        "type": "join",
-                                        "list": room[mess.room],
-                                        "join": 2,
-                                        "name": mess.name
-                                    });
-                                }
-                                ws.send({
-                                    "type": "setid",
-                                    'id': 2,
-                                    'name': mess.name,
-                                    'room': mess.room
-                                });
-                            }
-                        } else {
-                            room[mess.room] = [{
-                                'id': 1,
-                                'ws': ws
-                                }];
-                            for (var i = 0; i < room[mess.room].length; i++) {
-                                room[mess.room][i].ws.send({
-                                    "type": "join",
-                                    "list": room[mess.room],
-                                    "join": 1,
-                                    "name": mess.name
-                                });
-                            }
-                            ws.send({
-                                "type": "setid",
-                                'id': 1
-                            });
-                        }
-                        break;
-                    case "move":
-                        if (mess.hasOwnProperty('room')) {
-                            for (var i = 0; i < room[mess.room].length; i++) {
-                                room[mess.room][i].ws.send({
-                                    "type": "move",
-                                    "move": mess.move,
-                                    "name": mess.name,
-                                    "id": mess.id
-                                });
-                            }
-                        } else {
-                            ws.send({
+        console.log(JSON.stringify(message), message);
+        if (typeof message != Object) {
+            var mess = JSON.parse(message);
+        } else {
+            var mess = message;
+        }
+        console.log("mess", mess);
+        if (mess.hasOwnProperty('type')) {
+            console.log("mes-sdfs", mess.type, mess.room);
+            ////            if (mess.type == "join") {
+            //                if (room.hasOwnProperty(mess.room)) {
+            //
+            //                } else {
+            //                    room[mess.room] = []
+            ////                    roo{
+            ////                        'ws': ws,
+            ////                        id: 1,
+            ////                        name: mess.name
+            ////                    }];
+            //                    ws.send(JSON.stringify({ "type": "join", "join": 1, "name": mess.name, "room": mess.room}));
+            ////                    ws.send({
+            ////                        "type": "setid",
+            ////                        'id': 1,
+            ////                        "name": mess.name,
+            ////                        "room": mess.room
+            ////                    });
+            //              //  }
+            //            } else if (mess.type == "move") {
+            //
+            //            }
+            //        }
+
+            if (mess.hasOwnProperty('type')) {
+                switch (mess.type) {
+                case "join":
+                    if (room.hasOwnProperty(mess.room)) {
+                        if (room[mess.room].length > 1) {
+                            ws.send(JSON.stringify({
                                 "type": "error",
-                                'error': 'Please Use a Room'
-                            });
+                                'error': 'Room is Full'
+                            }));
+                        } else {
+                            var length = room[mess.room].length;
+                            room[mess.room][length - 1] = {
+                                'id': 2,
+                                'ws': ws
+                            };
+                            for (var i = 0; i < room[mess.room].length; i++) {
+                                room[mess.room][i].ws.send(JSON.stringify({
+                                    "type": "join",
+                                    // "list": room[mess.room],
+                                    "join": 2,
+                                    "name": mess.name
+                                }));
+                            }
+                            ws.send(JSON.stringify({
+                                "type": "setid",
+                                'id': 2,
+                                'name': mess.name,
+                                'room': mess.room
+                            }));
                         }
-                        break;
+                    } else {
+                        room[mess.room] = [{
+                            'id': 1,
+                            'ws': ws
+                                        }];
+                        for (var i = 0; i < room[mess.room].length; i++) {
+                            room[mess.room][i].ws.send(JSON.stringify({
+                                "type": "join",
+                                // "list": room[mess.room],
+                                "join": 1,
+                                "name": mess.name
+                            }));
+                        }
+                        ws.send(JSON.stringify({
+                            "type": "setid",
+                            'id': 1,
+                            'name': mess.name,
+                            'room': mess.room
+                        }));
                     }
+                    break;
+                case "move":
+                    if (mess.hasOwnProperty('room')) {
+                        for (var i = 0; i < room[mess.room].length; i++) {
+                            room[mess.room][i].ws.send(JSON.stringify({
+                                "type": "move",
+                                "move": mess.move,
+                                "name": mess.name,
+                                "id": mess.id
+                            }));
+                        }
+                    } else {
+                        ws.send(JSON.stringify({
+                            "type": "error",
+                            'error': 'Please Use a Room'
+                        }));
+                    }
+                    break;
+                }
             }
-//        } else {
-//            console.log("no object");
-//        }
+        } else {
+            console.log("no object");
+        }
         // wss.broadcast(message);
     });
 });
